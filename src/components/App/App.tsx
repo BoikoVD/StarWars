961e89d01918cb8f8b574d48e4df633d82627d40
BoikoVD <boiko.volodymyr.dm@gmail.com>
2024-07-28 13:32:18 +0200

    Cleanup

---
+++ src/components/App/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <main>
+      <header>
+        
+      </header>
+      <section>
+        Hello World!
+      </section>
+    </main>
+  );
+}
+
+export default App;
