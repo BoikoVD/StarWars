e211cc23acc0b9b4a890b12b684474f2b8a77724
BoikoVD <boiko.volodymyr.dm@gmail.com>
2024-07-29 18:25:18 +0200

    Add starfield bg

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,12 +1,14 @@
 import styles from './Layout.module.css';
 import { Outlet } from 'react-router-dom';
 import { Header } from './Header/Header';
+import { StarfieldBg } from '../StarfieldBg/StarfieldBg';
 
 export function Layout() {
   return (
     <div className={styles.layout}>
       <Header />
       <Outlet />
+      <StarfieldBg/>
     </div>
   );
 };
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { Layout } from './Layout/Layout';
 export { PeopleList } from './PeopleList/PeopleList';
 export { PersonDetailsFlow } from './PersonDetailsFlow/PersonDetailsFlow';
+export { StarfieldBg } from './StarfieldBg/StarfieldBg';
